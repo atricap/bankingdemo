62d073e67fe93f40b02a840db9886768aa0d911f
atricap <172540644+atricap@users.noreply.github.com>
2024-06-29 21:51:22 +0200

    Add project files from Spring Initializr

---
+++ src/test/java/de/atricap/bankingdemo/BankingDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package de.atricap.bankingdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
